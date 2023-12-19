0b2e7c01bd7c003ddbc483d77c7328df947be6d9
Pablo <jhintecarrea1237@gmail.com>
2023-12-19 09:18:37 +0100

    cambios css

--- carrusel.js
+++ carrusel.js
@@ -18,3 +18,33 @@ function mostrarImagen() {
 
   carruselContainer.style.transform = `translateX(${-currentIndex * carruselWidth}px)`;
 }
+
+// Añade la siguiente parte para hacer el carrusel automático
+// Obtén la referencia al contenedor del carrusel
+var carruselContainer = document.querySelector('.carrusel-container');
+
+// Función para cambiar la imagen del carrusel
+function cambiarImagen(n) {
+    currentIndex += n;
+    mostrarImagen();
+}
+
+// Función para mostrar la imagen actual
+function mostrarImagen() {
+    var carruselItems = document.querySelectorAll('.carrusel-item');
+    
+    // Verifica los límites del índice
+    if (currentIndex >= carruselItems.length) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = carruselItems.length - 1;
+    }
+
+    // Calcula la transformación para mostrar la imagen actual
+    var transformValue = -currentIndex * 100 + '%';
+    carruselContainer.style.transform = 'translateX(' + transformValue + ')';
+}
+// Establece un intervalo para cambiar automáticamente las imágenes cada 5 segundos
+setInterval(function () {
+    cambiarImagen(1);
+}, 5000);
